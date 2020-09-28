dda8848985b252d0fa33bcb4b74a00d1aa0dc34e
lmk0420 <lmk19960420@gmail.com>
2020-09-28 21:50:22 +0900

    [delete]不要な記述を削除

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,7 +1,17 @@
 import React, { FC } from 'react'
+import Link from 'next/link'
 
 const FirstPost: FC = () => {
-  return <h1>First Post</h1>
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
 }
 
 export default FirstPost
